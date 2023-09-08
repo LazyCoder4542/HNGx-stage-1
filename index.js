037f443796b0e8081387e48e4532ec9f3697f1c8
Adeola Fadare <adeolafadare8@gmail.com>
2023-09-08 16:31:47 +0100

    fix: dynamic UTC

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const DAY = new Date().getDay()
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 document.querySelector("[data-testid='currentDayOfTheWeek']").textContent = days[DAY]
+document.querySelector("[data-testid='currentUTCTime']").textContent = new Date().getTime()
 setInterval(()=> {
     let UTC = new Date().getTime()
     document.querySelector("[data-testid='currentUTCTime']").textContent = UTC
